ce9a8c9d8bf2d2ac0ff4da8024f12b85a9c4fc58
phoenix17ux <62536540+phoenix17ux@users.noreply.github.com>
2020-05-02 00:12:08 +0300

    Update

--- gauss.py
+++ gauss.py
@@ -1,41 +1,31 @@
 import tkinter as tk
 import os
+import sys
 
 class GaussApp(tk.Frame):
-    def __init__(self, parent, rank):
+    def __init__(self, parent):
         tk.Frame.__init__(self, parent)
-        self.rank = rank
+        self.rank = 0
         self.dict_var = {}
         self.text = ''
         self.matrix = []
-        
-        for row in range(rank):
-            for column in range(rank + 1):
+        self.butRead = tk.Button(self, text='Прочитать файл', command=self.readFile)
+        self.butRead.grid(row=0 , column=2, sticky=tk.E)
+        self.x = tk.Entry(self, width=10, font='Arial 16')
+        self.x.grid(row=0, column=0, sticky=tk.E)
+        self.xB = tk.Button(self, text='Ввести ранг', command=self.choiceRank)
+        self.xB.grid(row=0, column=1, sticky=tk.E)
+
+    def createMatrix(self):    
+        for row in range(self.rank):
+            for column in range(self.rank + 1):
                 index = (row, column)
                 pole = tk.Entry(self, width=10, font='Arial 16')
-                pole.grid(row=row, column=column, sticky=tk.E)
+                pole.grid(row=row, column=column, sticky=tk.W)
                 self.dict_var[index] = pole
 
         
-        butOne = tk.Button(self, text='Прямой ход', command=self.method1)
-        butOne.grid(row=rank+2 , column=0, sticky=tk.E)
-
-        butTwo = tk.Button(self, text='Обратный ход', command=self.method2)
-        butTwo.grid(row=rank+2 , column=1, sticky=tk.E)
-        
-        butRead = tk.Button(self, text='Прочитать файл', command=self.readFile)
-        butRead.grid(row=rank+2 , column=2, sticky=tk.E)
-
-
-        self.text_tk = tk.Text(width=50, height=10)
-        self.text_tk.pack()
-        frame = tk.Frame()
-        frame.pack()
-        b_insert = tk.Button(frame, text="Решение", command=self.insertText)
-        b_insert.pack(side=tk.LEFT)
-
-
-    def makeMatrix(self):
+    def getMatrix(self):
         for i in range(self.rank):
             self.matrix.append([])
             for j in range(self.rank + 1):
@@ -75,8 +65,22 @@ class GaussApp(tk.Frame):
         self.text += "------------------------------\n"
 
 
+    def panel(self):
+        butOne = tk.Button(self, text='Прямой ход', command=self.method1)
+        butOne.grid(row=self.rank+1 , column=0, sticky=tk.W)
+
+        butTwo = tk.Button(self, text='Обратный ход', command=self.method2)
+        butTwo.grid(row=self.rank+1 , column=1, sticky=tk.W)
+        self.text_tk = tk.Text(width=50, height=10)
+        self.text_tk.pack()
+        frame = tk.Frame()
+        frame.pack()
+        b_insert = tk.Button(self, text="Решение", command=self.insertText)
+        b_insert.grid(row=self.rank+1 , column=2, sticky=tk.W)
+    
+
     def method1(self):
-        self.makeMatrix()
+        self.getMatrix()
         for i in range(self.rank):
             a = self.matrix[i]
             self.solve()
@@ -97,7 +101,7 @@ class GaussApp(tk.Frame):
 
 
     def method2(self):
-        self.makeMatrix()
+        self.getMatrix()
         for i in range(self.rank - 1, -1, -1):
             self.solve()
             line = []
@@ -122,13 +126,30 @@ class GaussApp(tk.Frame):
         self.insertMatrix()
 
 
+    def choiceRank(self):
+        self.rank = int(self.x.get())
+        self.butRead.destroy()
+        self.x.destroy()
+        self.xB.destroy()
+
+        self.panel()
+        self.createMatrix()
+
+
     def readFile(self):
-        file = open((os.getcwd() + "/file.txt"), 'r')
+        self.rank = 0
+        
+        if sys.platform[:3] == "win":
+            file = open(os.getcwd() + "\\file.txt", 'r')
+        else:
+            file = open(os.getcwd() + "/file.txt", 'r')
         line = []
         for i in file.read():
             if i.isdigit():
-                line.append(int(i))    
-
+                line.append(int(i)) 
+            else:
+                self.rank +=1   
+        
         self.matrix = []
 
         for i in range(self.rank):
@@ -136,25 +157,14 @@ class GaussApp(tk.Frame):
             for j in range(self.rank + 1):
                 self.matrix[i].append(line[j])
             line = line[self.rank+1:]
+        self.createMatrix()
         self.insertMatrix()
+        self.panel()
 
 
 if __name__ == "__main__":
-    def show_message():
-        global rank
-        rank = int(x.get())
-        rang.destroy()
-
-
-    rang = tk.Tk()
-    rang.title("Ранг матрицы")
-    x = tk.Entry(rang, width=10, font='Arial 16')
-    x.grid(row=0, column=0, sticky=tk.E)
-    xB = tk.Button(rang, text='ввод', command=show_message)
-    xB.grid(row=1, column=0, sticky=tk.E)
-    rang.mainloop()
 
     app = tk.Tk()
     app.title("Метод Гаусса")
-    GaussApp(app, rank).pack()
+    GaussApp(app).pack()
     app.mainloop()
